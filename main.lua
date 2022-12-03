61b84c75db3c29d5959f0d95f691c8a2d9ed2e3b
Marcelo <marcelogamedev99@gmail.com>
2022-12-03 20:44:03 +0000

    Pong0

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
-
+--Start Method
 function love.load()
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
@@ -9,7 +9,7 @@ function love.load()
     }
 )
 end
-
+--Draw graphics
 function love.draw()
     love.graphics.printf('Hello Pong', 0, WINDOW_HEIGHT / 2 - 6, WINDOW_WIDTH, 'center')
 end
